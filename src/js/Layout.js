12d9de152b5e0c831228bc14020928ce5cdddc23
Arjan Jassal <arjan@ettrics.com>
2016-01-27 20:08:27 -0800

    add public method to append more images

--- src/js/Layout.js
+++ src/js/Layout.js
@@ -2,15 +2,19 @@ import imagesLoaded from 'imagesloaded';
 
 const Layout = ((imagesLoaded) => {
 
-  let maxHeight = 300;
+  const defaults = {
+    loadedClassName: 'is-loaded',
+    maxHeight: 250
+  };
 
   class Layout {
 
     constructor(elem, opts) {
       this._elem = elem;
       this._images = [].slice.call(this._elem.querySelectorAll('img'));
-      this._opts = opts || {};
+      this._opts = Object.assign(defaults, opts);
       this.init();
+      this.appendImages = this._loadNewImages;
     }
 
     init() {
@@ -19,20 +23,23 @@ const Layout = ((imagesLoaded) => {
       this._loadImages();
     }
 
+    _loadNewImages(img) {
+      img.forEach((img) => {
+        this._images.push(img);
+      });
+      this._loadImages();
+    }
+
     _initOptions() {
-      if (this._opts.hasOwnProperty('maxHeight')) {
-        if (typeof this._opts.maxHeight === 'number') {
-          maxHeight = this._opts.maxHeight;
-        } else {
-          throw new Error(`maxHeight must be a number not a ${typeof this._opts.beforeLoad}.`);
-        }
+      if (typeof this._opts.maxHeight !== 'number') {
+        console.error(`maxHeight must be a number not a ${typeof this._opts.maxHeight}.`);
       }
 
       if (this._opts.hasOwnProperty('beforeLoad')) {
         if (typeof this._opts.beforeLoad === 'function') {
           this._beforeLoad = this._opts.beforeLoad;
         } else {
-          throw new Error(`beforeLoad must be a function not a ${typeof this._opts.beforeLoad}.`);
+          console.error(`beforeLoad must be a function not a ${typeof this._opts.beforeLoad}.`);
         }
       }
 
@@ -40,12 +47,14 @@ const Layout = ((imagesLoaded) => {
         if (typeof this._opts.afterLoad === 'function') {
           this._afterLoad = this._opts.afterLoad;
         } else {
-          throw new Error(`afterLoad must be a function not a ${typeof opts.afterLoad}.`);
+          console.error(`afterLoad must be a function not a ${typeof opts.afterLoad}.`);
         }
       }
     }
 
     _addEventListeners() {
+
+      // Re-calculate image dimensions when the window is resized.
       window.addEventListener('resize', this._createRows.bind(this, this._images));
     }
 
@@ -59,22 +68,33 @@ const Layout = ((imagesLoaded) => {
         if (this._beforeLoad) this._beforeLoad();
 
         // Create rows of images.
-        this._createRows(this._images);
+        this._createRows(this._images, this._elem.clientWidth);
 
         // Call afterLoad().
         if (this._afterLoad) this._afterLoad();
 
         // Stagger animation of each image.
-        instance.images.forEach((img) => {
+        instance.images.forEach((image) => {
+          let img = image.img;
           delay += 0.025;
-          img.img.style.animationDelay = `${delay}s`;
-          img.img.classList.add('is-loaded');
-          this._animationComplete(img.img);
+
+          if (!img.classList.contains(this._opts.loadedClassName)) {
+            img.style.animationDelay = `${delay}s`;
+            img.classList.add(this._opts.loadedClassName);
+          }
+          
+          this._animationComplete(img);
+
+          // Log error to console if image fails to load.
+          if (!image.isLoaded) {
+            console.error(`${image.img.src} failed to load.`);
+          }
         });
       });
 
       imgLoad.on('progress', (instance, image) => {
         let img = image.img;
+        console.log('a');
         img.setAttribute('data-width', img.offsetWidth);
         img.setAttribute('data-height', img.offsetHeight);
       });
@@ -82,23 +102,23 @@ const Layout = ((imagesLoaded) => {
 
     _createRows(images) {
       const containerWidth = this._elem.clientWidth;
-      let imgs = this._images.slice();
+      let imgs = images.slice();
       let slice, height;
 
       loop: while (imgs.length > 0) {
 
         for (let i = 1; i <= imgs.length; i++) {
           slice = imgs.slice(0, i);
-          height = this._getHeight(slice);
+          height = this._getHeight(slice, containerWidth);
 
-          if (height < maxHeight) {
+          if (height < this._opts.maxHeight) {
             this._setDimensions(slice, height);
             imgs = imgs.slice(i);
             continue loop;
           }
         }
 
-        this._setDimensions(slice, Math.min(maxHeight, height));
+        this._setDimensions(slice, Math.min(this._opts.maxHeight, height));
         break;
       }
     }
@@ -114,8 +134,7 @@ const Layout = ((imagesLoaded) => {
       }
     }
 
-    _getHeight(images) {
-      let containerWidth = this._elem.clientWidth;
+    _getHeight(images, containerWidth) {
       let val = 0;
 
       if (images && images.length) {
